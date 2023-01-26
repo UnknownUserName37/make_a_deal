de54e888a46f778c7d110bbcd1ea6322d31612a1
Nappa <kikbox3112@gmail.com>
2023-01-26 23:51:38 +0300

    Add new file with shortly test.

--- tests/test_second_variant.py
+++ tests/test_second_variant.py
@@ -7,18 +7,12 @@ def test_eto_pizda_test(setup):
 
     general_window = driver.current_window_handle
 
-    ALL_THEMES = driver.find_elements(By.XPATH, "//div[@class = 'title']")
-    themes_list = [themes_list for themes_list in ALL_THEMES]
-
-    # action = ActionChains(driver)
+    all_themes = driver.find_elements(By.XPATH, "//div[@class = 'title']")
+    themes_list = [themes_list for themes_list in all_themes]
 
     for one_theme in range(len(themes_list)):
-        text_for_assert = None
 
         ActionChains(driver).key_down(Keys.LEFT_CONTROL).click(themes_list[one_theme]).perform()
-
-        # handles = driver.window_handles[1]
-        # driver.switch_to.window(handles)
         driver.switch_to.window(driver.window_handles[1])
         text_for_assert = driver.find_element(By.XPATH, "//h1/i").text
 
