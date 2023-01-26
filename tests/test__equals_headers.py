122b6562f50997c11a9b9fe03dd63ac452d50543
Nappa <kikbox3112@gmail.com>
2023-01-26 14:05:31 +0300

    lol
    5 min.
    
    Ohhhhhhh
    
    One Change.

--- tests/test__equals_headers.py
+++ tests/test__equals_headers.py
@@ -1,12 +1,3 @@
-# from locators.locators import Locators
-# from pages.form_page import FormPage
-#
-# #  Допилим.
-# class TestPizdaTest:
-#
-#     def test_eto_pizda_test(self, start_driver):
-#         all_thems = FormPage.all_elements_what_we_are_need
-#         print(all_thems)
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 
