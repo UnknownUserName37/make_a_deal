b497d72215c99c60467d08683c7c66d9fa36eaa4
Nappa <kikbox3112@gmail.com>
2023-01-26 16:31:15 +0300

    Revert "Delete pages directory"
    This reverts commit 2472d360e6478fcec351daab42f1ef65cc4c4f35.

---
+++ pages/form_page.py
@@ -0,0 +1,11 @@
+# from locators.locators import Locators
+# from pages.base_page import BasePage
+#
+#
+# class FormPage(BasePage):
+#
+#     def all_elements_what_we_are_need(self):
+#         thems_list = self.elements_are_visible(Locators.LIST_OF_THEMES).text
+#         result_list = [i.text for i in thems_list]
+#         print(result_list)
+#         return result_list
