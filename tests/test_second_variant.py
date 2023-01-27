674d6f51cc517b9601d888b014daf7da207aca59
Nappa <kikbox3112@gmail.com>
2023-01-27 23:44:18 +0300

    Add new file with shortly test.
    Change new file. Do shortly.
    
    Return condition of native file. Make him long. Again. )
    
    Add comment about ways in a top of files with tests.
    
    I' m understand my mistake.
    
    Fix text_for_assert.text. It's incorrect.
    
    Uncomment. Micro_pep.
    
    I RESTORE IT!
    
    100% commit
    
    New Branch for changes.

--- tests/test_second_variant.py
+++ tests/test_second_variant.py
@@ -1,18 +1,19 @@
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
+
+
 # Second way
 
 
 def test_eto_pizda_test(setup):
     driver = setup
-    general_window, all_themes = driver.current_window_handle, driver.find_elements(By.XPATH, "//div[@class = 'title']")
-    themes_list = [themes_list for themes_list in all_themes]
+    general_window = driver.current_window_handle
+    headers_off_all_articles = driver.find_elements(By.XPATH, "//div[@class = 'title']")
 
-    for one_theme in range(len(themes_list)):
-        ActionChains(driver).key_down(Keys.LEFT_CONTROL).click(themes_list[one_theme]).perform()
+    for header_of_one_article in headers_off_all_articles:
+        text_of_one_header = header_of_one_article.text
+        ActionChains(driver).key_down(Keys.LEFT_CONTROL).click(header_of_one_article).perform()
         driver.switch_to.window(driver.window_handles[1])
-        text_for_assert = driver.find_element(By.XPATH, "//h1/i").text
         driver.close()
         driver.switch_to.window(general_window)
-
-        assert text_for_assert == themes_list[one_theme].text
+        assert driver.find_element(By.XPATH, "//h1/i").text == text_of_one_header
