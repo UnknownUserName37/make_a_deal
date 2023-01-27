6509057d17b1c616672a26414f13ca1dfc571542
Nappa <kikbox3112@gmail.com>
2023-01-28 00:11:25 +0300

    Add new file with shortly test.
    Change new file. Do shortly.
    
    Return condition of native file. Make him long. Again. )
    
    Add comment about ways in a top of files with tests.
    
    I' m understand my mistake.
    
    Fix text_for_assert.text. It's incorrect.
    
    Uncomment. Micro_pep.
    
    I RESTORE IT!
    
    100% commit
    
    New Branch for changes.
    
    New day - new knowledge.

--- tests/on_a_future.py
+++ tests/on_a_future.py
@@ -1,9 +1,9 @@
-# from locators.locators import Locators
-# from pages.form_page import FormPage
-#
-# #  Допилим.
-# class TestPizdaTest:
-#
-#     def test_eto_pizda_test(self, start_driver):
-#         all_thems = FormPage.all_elements_what_we_are_need
-#         print(all_thems)
+from locators.locators import Locators
+from pages.form_page import FormPage
+
+#  Допилим.
+class TestPizdaTest:
+
+    def test_eto_pizda_test(self, start_driver):
+        all_thems = FormPage.all_elements_what_we_are_need
+        print(all_thems)
