0b7ef7d5cb1b474f854b79d3b4cc6937a5b411a1
Nappa <kikbox3112@gmail.com>
2023-01-27 13:04:54 +0300

    Add new file with shortly test.
    Change new file. Do shortly.
    
    Return condition of native file. Make him long. Again. )
    
    Add comment about ways in a top of files with tests.
    
    I' m understand my mistake.

--- tests/test_second_variant.py
+++ tests/test_second_variant.py
@@ -2,20 +2,17 @@ from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 # Second way
 
+
 def test_eto_pizda_test(setup):
     driver = setup
-    general_window = driver.current_window_handle
-
-    all_themes = driver.find_elements(By.XPATH, "//div[@class = 'title']")
+    general_window, all_themes = driver.current_window_handle, driver.find_elements(By.XPATH, "//div[@class = 'title']")
     themes_list = [themes_list for themes_list in all_themes]
 
     for one_theme in range(len(themes_list)):
-
         ActionChains(driver).key_down(Keys.LEFT_CONTROL).click(themes_list[one_theme]).perform()
         driver.switch_to.window(driver.window_handles[1])
-        text_for_assert = driver.find_element(By.XPATH, "//h1/i")
-
+        text_for_assert = driver.find_element(By.XPATH, "//h1/i").text
         driver.close()
         driver.switch_to.window(general_window)
 
-        assert text_for_assert.text == themes_list[one_theme].text
+        assert text_for_assert == themes_list[one_theme].text
