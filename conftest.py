69fc0daeba82dcfa6a41cb069bfed7466f1c0b0b
Nappa <kikbox3112@gmail.com>
2023-01-28 01:47:21 +0300

    I want to have this files just on a local.
    hmmmmm........

--- conftest.py
+++ conftest.py
@@ -2,21 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options as chrome_options
-
-
-# @pytest.fixture
-# def fixture_for_set_params():
-#     options = chrome_options()
-#     options.add_argument('chrome')
-#     options.add_argument('--start-maximized')
-#     options.add_argument('--headless')
-#     return options
 
 
 @pytest.fixture
 def fixture_for_set_driver():
-    # options = fixture_for_set_params
     driver_service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=driver_service)
     return driver
