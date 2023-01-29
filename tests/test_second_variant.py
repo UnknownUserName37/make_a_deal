eeced2f3776a6d03be56ff02d184403c56aeaeff
Nappa <kikbox3112@gmail.com>
2023-01-29 16:09:07 +0300

    And again.

--- tests/test_second_variant.py
+++ tests/test_second_variant.py
@@ -2,7 +2,7 @@ from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 
 
-# Second way. LoudPass
+# Second way. LoudPass123
 
 
 def test_eto_pizda_test(setup):
